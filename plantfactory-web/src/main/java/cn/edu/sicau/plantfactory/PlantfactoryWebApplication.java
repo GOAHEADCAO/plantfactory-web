98f03296eabaa74f5a57d00c018bf7305f19e0da
HUJIANG\jianghui <jianghui@hujiang.com>
2019-03-18 20:07:07 +0800

    init

---
+++ plantfactory-web/src/main/java/cn/edu/sicau/plantfactory/PlantfactoryWebApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.sicau.plantfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantfactoryWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantfactoryWebApplication.class, args);
+    }
+
+}
